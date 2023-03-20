e33c58f9965d2d985035b6f3c71999baf35677b1
tobiaskleber11 <tobiaskleber8@gmail.com>
2023-03-20 16:05:35 -0400

    settings

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'restaurant',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
